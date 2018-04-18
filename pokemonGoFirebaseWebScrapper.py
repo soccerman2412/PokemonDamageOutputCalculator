77b8085adcca747996fc3298ae5ab873048f3409
Taylor Plimpton <taylor.plimpton@smallplanet.com>
2018-04-18 18:22:57 -0400

    (WIP) Temp Method

--- pokemonGoFirebaseWebScrapper.py
+++ pokemonGoFirebaseWebScrapper.py
@@ -1,5 +1,29 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+import requests
+from bs4 import BeautifulSoup
+
+cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+
+# def scrapePokemonStats(siteURL):
+#     response = requests.get(url)
+#     html = response.content
+
+#     soup = BeautifulSoup(html)
+#     table = soup.find('tbody', attrs={'class': 'stripe'})
+
+#     list_of_rows = []
+#     for row in table.findAll('tr'):
+#         list_of_cells = []
+#         for cell in row.findAll('td'):
+#             text = cell.text.replace('&nbsp;', '')
+#             list_of_cells.append(text)
+#         list_of_rows.append(list_of_cells)
+
+#     print list_of_rows
